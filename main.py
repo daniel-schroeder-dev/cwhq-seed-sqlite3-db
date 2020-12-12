1989264bc5c2c912754802df3fd82cc0e5615742
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-12 12:02:27 -0600

    Get 100 users from the randomuser.me API.

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import requests
 
 def get_random_users():
     api_url = "https://randomuser.me/api/"
+    api_query = {
+        "results": 100,
+    }
 
-    response = requests.get(api_url)
+    response = requests.get(api_url, api_query)
     if response.status_code >= 200 and response.status_code < 300:
         json_data = response.json()
         users = json_data["results"]
@@ -17,6 +20,6 @@ if __name__ == "__main__":
     users = get_random_users()
 
     if users:
-        print(users)
+        print(len(users))
     else:
         print("Something went wrong!")
